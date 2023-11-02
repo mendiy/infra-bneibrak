4d23f80e7e6ff03add76d2e49edc0132ca8960d3
mendi <mendiby@gmail.com>
2023-11-02 15:57:45 +0200

    create login/register pages

---
+++ src/login.jsx
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F6C927'
+    },
+    background: {default: '#0A0A1B'}
+  }
+});;
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor:'#F6C927' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" color='#F6C927'>
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} color='#F6C927'>
+            <TextField
+
+              margin="normal"
+              color="primary"
+              required
+              fullWidth
+              id="email"
+              name="email"
+              
+              autoComplete="email"
+              
+              inputProps={{
+              placeholder: "Email Address",
+              style: { color: '#F6C927', placeholder: '#FF5733', background: '#21213E'} 
+            }}
+              autoFocus
+            
+            />
+            <TextField
+              margin="normal"
+              color="primary"
+              required
+              fullWidth
+              name="password"
+              
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              inputProps={{
+              placeholder: "Password",
+              style: { color: '#F6C927', placeholder: '#FF5733', background: '#21213E'} 
+            }}
+            />
+            <FormControlLabel
+            
+              control={<Checkbox value="remember" label='Primary' color='primary' />}
+              label="Remember me" 
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
