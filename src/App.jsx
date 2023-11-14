d2f6f69cdb63c5f5650ca79e2f8ab33d523aa5b0
mendi <mendiby@gmail.com>
2023-11-14 09:56:57 +0200

    add App file

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import React from "react";
 import SignIn from "./login";
 import SignUp from "./register";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F6C927'
+    },
+    background: {default: '#0A0A1B'}
+  }
+});;
 
 
 export default function App() {
     return (
-    <Router>
-        <Routes>
-        <Route exact path="/register"
-          element ={<SignUp />} />
-        <Route path="/" element={<SignIn />} /> 
-        </Routes>
-    </Router>
+      <ThemeProvider theme={theme}>
+      <Router>
+          <Routes>
+          <Route exact path="/register"
+            element ={<SignUp />} />
+          <Route path="/" element={<SignIn />} /> 
+          </Routes>
+      </Router>
+      </ThemeProvider>
     )
   }
     
