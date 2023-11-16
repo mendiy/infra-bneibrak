40929e2fed4650ae581440e4e89ba7e2fd6bad97
mendi <mendiby@gmail.com>
2023-11-16 15:03:08 +0200

    add package.json

--- src/token.js
+++ src/token.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const tokenIsCorrect = async () => {
     const secretKey = 'megobb';
+    // const token = localStorage.getItem('authToken')
     const token = 'eyJhbGciOiJIUzI1NiJ9.aGhAampqampqampqampqampqamouY29t.UlR8638RE8BEet0A-gXX4CjFi95x4-1ENnOmvvk1L8g';
     const decoded = jwt.verify(token, secretKey);
     if (decoded) {
